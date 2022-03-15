07da2366014957aef84d2feccdf7407b8cb3e133
BloodyMary8 <masharabota1996@gmail.com>
2022-03-15 20:21:07 +0700

    Initial commit

---
+++ src/Merchant.java
@@ -0,0 +1,12 @@
+public class Merchant {
+    public String sell(Goods goods){
+        String result = "";
+        if (goods == Goods.POTION){
+            result = "potion";
+        }
+        return result;
+    }
+    public enum Goods{
+        POTION
+    }
+}
---
+++ src/Skeleton.java
@@ -0,0 +1,6 @@
+public class Skeleton extends Character{
+    public Skeleton(String name, int health, int strength, int dexterity, int xp, int gold) {
+        super(name, health, strength, dexterity, xp, gold);
+    }
+}
+
